18ee26f999924fd2b6375a1aa0e40950eb3d4258
Isaura Martí <imarti.teixido@gmail.com>
2023-01-05 16:26:41 +0100

    functions to edit, delete, open & close modal

--- app.js
+++ app.js
@@ -9,17 +9,21 @@ const descriptionEvent = document.querySelector('[name="description"]');
 const typeOfEvent = document.querySelector('[name="type"]');
 const btnCancelModalAddEvent = document.getElementById("cancelModalAddEvent");
 const btnCreateEvent = document.getElementById("createEvent");
+const btnDeleteEvent = document.getElementById("deleteEvent");
+const btnEditEvent = document.getElementById("editEvent");
 const leftArrow = document.getElementById("leftArrow");
 const rightArrow = document.getElementById("rightArrow");
+const btnAddNewEvent = document.getElementById("addNewEvent");
+const modalAddEditEvent = document.querySelector(".modal-add-edit-event");
+const btnCloseModal = document.getElementById("closeModal");
+const containerMainPage = document.querySelector(".container-main-page");
+const modalButtonsAdd = document.querySelector(".modal-container-btn-add");
+const modalButtonsEdit = document.querySelector(".modal-container-btn-edit");
 
 const todayDate = new Date();
-let [month, day, year] = [
-  todayDate.getMonth(),
-  todayDate.getDate(),
-  todayDate.getFullYear(),
-];
+let [month, year] = [todayDate.getMonth(), todayDate.getFullYear()];
 
-let newEvent = {};
+let currentEvent = {};
 let arrOfEvents = [];
 
 let numberOfDaysMonth;
@@ -40,23 +44,22 @@ const arrMonths = [
   "December",
 ];
 
-window.addEventListener("load", getActualMonth);
-window.addEventListener("load", getDaysInMonth());
-window.addEventListener("load", printDaysOnCalendar());
-window.addEventListener("load", printEventsOnCalendar());
+window.addEventListener("load", printDaysOnCalendar);
+window.addEventListener("load", printEventsOnCalendar);
+window.addEventListener("keydown", closeModalWithEscapeKey);
 btnCreateEvent.addEventListener("click", addNewEvent);
+btnEditEvent.addEventListener("click", saveChangesEditedEvent);
 leftArrow.addEventListener("click", moveTowardsLeft);
 rightArrow.addEventListener("click", moveTowardsRight);
-
-function getActualMonth() {
-  todayText.innerText = `${arrMonths[month]} ${year}`;
-}
-
-function getDaysInMonth() {
-  numberOfDaysMonth = new Date(year, month + 1, 0).getDate();
-}
+btnAddNewEvent.addEventListener("click", startCreateNewEvent);
+btnDeleteEvent.addEventListener("click", deleteEvent);
+btnCancelModalAddEvent.addEventListener("click", closeModal);
+btnCloseModal.addEventListener("click", closeModal);
 
 function printDaysOnCalendar() {
+  todayText.innerText = `${arrMonths[month]} ${year}`;
+  numberOfDaysMonth = new Date(year, month + 1, 0).getDate();
+
   const dayOfWeek = new Date(year, month, 1).getDay();
   if (dayOfWeek > 0) {
     divStartPrint = dayOfWeek - 1;
@@ -87,13 +90,7 @@ function printEventsOnCalendar() {
     });
 
     arrOfEventsToPrint.forEach((el) => {
-      const eventToPrint = document.createElement("div");
-      eventToPrint.setAttribute("data-event-id", el.id);
-      eventToPrint.addEventListener("click", openEvent);
-      eventToPrint.textContent = el.title;
-      eventToPrint.style.backgroundColor = el.color;
-      const day = new Date(el.initialDate).getDate();
-      daysOfCalendar[day + divStartPrint - 1].appendChild(eventToPrint);
+      createDivToShowEvent(el);
     });
   } else {
     arrOfEvents = [];
@@ -102,44 +99,107 @@ function printEventsOnCalendar() {
 
 function printNewEventOnCalendar() {
   if (
-    new Date(newEvent.initialDate).getFullYear() === year &&
-    new Date(newEvent.initialDate).getMonth() === month
+    new Date(currentEvent.initialDate).getFullYear() === year &&
+    new Date(currentEvent.initialDate).getMonth() === month
   ) {
-    const eventToPrint = document.createElement("div");
-    eventToPrint.setAttribute("data-event-id", newEvent.id);
-    eventToPrint.addEventListener("click", openEvent);
-    eventToPrint.textContent = newEvent.title;
-    eventToPrint.style.backgroundColor = newEvent.color;
-    const day = new Date(newEvent.initialDate).getDate();
-    daysOfCalendar[day + divStartPrint - 1].appendChild(eventToPrint);
+    createDivToShowEvent(currentEvent);
   }
 }
 
-function openEvent(e) {
-  console.log(arrOfEvents);
+function createDivToShowEvent(event) {
+  const eventToPrint = document.createElement("div");
+  eventToPrint.setAttribute("data-event-id", event.id);
+  eventToPrint.addEventListener("click", openEvent);
+  eventToPrint.textContent = event.title;
+  eventToPrint.style.backgroundColor = event.color;
+  const day = new Date(event.initialDate).getDate();
+  daysOfCalendar[day + divStartPrint - 1].appendChild(eventToPrint);
+}
 
+function openModal() {
+  modalAddEditEvent.classList.add("modal-active");
+  containerMainPage.classList.add("background-modal-active");
+}
+
+function closeModal() {
+  modalAddEditEvent.classList.remove("modal-active");
+  containerMainPage.classList.remove("background-modal-active");
+  modalButtonsAdd.className = "modal-container-btn-add";
+  modalButtonsEdit.className = "modal-container-btn-edit";
+  currentEvent = {};
+}
+
+function closeModalWithEscapeKey(e) {
+  if (e.code === "Escape") closeModal();
+}
+
+function openEvent(e) {
   const eventToOpen = arrOfEvents.filter((el) => {
     return el.id == e.target.dataset.eventId;
   });
 
-  console.log(eventToOpen);
+  currentEvent = eventToOpen[0];
+
+  titleEvent.value = currentEvent.title;
+  datetimeStartEvent.value = currentEvent.initialDate;
+  datetimeFinishEvent.value = currentEvent.endDate;
+  timeRemindEvent.value = currentEvent.timeRemind;
+  colorEvent.value = currentEvent.color;
+  descriptionEvent.value = currentEvent.description;
+  typeOfEvent.value = currentEvent.type;
+
+  modalButtonsEdit.classList.add("btns-active");
+  openModal();
 }
 
 function addNewEvent(e) {
   e.preventDefault();
-  newEvent.id = Date.now();
-  newEvent.title = titleEvent.value;
-  newEvent.initialDate = datetimeStartEvent.value;
-  newEvent.endDate = datetimeFinishEvent.value;
-  newEvent.timeRemind = timeRemindEvent.value;
-  newEvent.color = colorEvent.value;
-  newEvent.description = descriptionEvent.value;
-  newEvent.type = typeOfEvent.value;
-  arrOfEvents.push(newEvent);
+  currentEvent.id = Date.now();
+  changeValuesCurrentEvent();
+  arrOfEvents.push(currentEvent);
   localStorage.setItem("events", JSON.stringify(arrOfEvents));
   printNewEventOnCalendar();
   resetModalValues();
-  newEvent = {};
+  closeModal();
+}
+
+function saveChangesEditedEvent(e) {
+  e.preventDefault();
+
+  changeValuesCurrentEvent();
+
+  for (let i = 0; i < arrOfEvents.length; i++) {
+    if (arrOfEvents[i].id === currentEvent.id) {
+      arrOfEvents[i] = currentEvent;
+    }
+  }
+
+  localStorage.setItem("events", JSON.stringify(arrOfEvents));
+
+  const printedEventToChange = document.querySelector(
+    `[data-event-id="${currentEvent.id}"]`
+  );
+
+  printedEventToChange.remove();
+
+  createDivToShowEvent(currentEvent);
+  resetModalValues();
+  closeModal();
+}
+
+function startCreateNewEvent() {
+  modalButtonsAdd.classList.add("btns-active");
+  openModal();
+}
+
+function changeValuesCurrentEvent() {
+  currentEvent.title = titleEvent.value;
+  currentEvent.initialDate = datetimeStartEvent.value;
+  currentEvent.endDate = datetimeFinishEvent.value;
+  currentEvent.timeRemind = timeRemindEvent.value;
+  currentEvent.color = colorEvent.value;
+  currentEvent.description = descriptionEvent.value;
+  currentEvent.type = typeOfEvent.value;
 }
 
 function resetModalValues() {
@@ -150,6 +210,21 @@ function resetModalValues() {
   colorEvent.value = "";
   descriptionEvent.value = "";
   typeOfEvent.value = "";
+}
+
+function deleteEvent(e) {
+  e.preventDefault();
+
+  arrOfEvents = arrOfEvents.filter((event) => event.id !== currentEvent.id);
+  localStorage.setItem("events", JSON.stringify(arrOfEvents));
+
+  const printedEventToDelete = document.querySelector(
+    `[data-event-id="${currentEvent.id}"]`
+  );
+
+  printedEventToDelete.remove();
+  resetModalValues();
+  closeModal();
 }
 
 function deletePrintedDays() {
@@ -172,8 +247,6 @@ function moveTowardsLeft() {
     month -= 1;
   }
   deletePrintedDays();
-  getActualMonth();
-  getDaysInMonth();
   printDaysOnCalendar();
   printEventsOnCalendar();
 }
@@ -192,8 +265,6 @@ function moveTowardsRight() {
     month += 1;
   }
   deletePrintedDays();
-  getActualMonth();
-  getDaysInMonth();
   printDaysOnCalendar();
   printEventsOnCalendar();
 }
